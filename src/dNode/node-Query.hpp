e50cc99f47a7e9cc634b13121a19b3792a3a102f
Ariston Darmayuda <darmayuda.b.ariston@gmail.com>
2017-06-22 17:06:58 +0800

    mod: node-Query JSON processing init: node-Response for node-Query

--- src/dNode/node-Query.hpp
+++ src/dNode/node-Query.hpp
@@ -10,7 +10,7 @@ private:
   std::string _command;
   std::string _data;
 public:
-  static nodeQuery* parse(std::string json){
+  static nodeQuery* fromJSON(std::string json){
     std::string _module = "";
     std::string _subModule = "";
     std::string _command = "";
@@ -19,7 +19,7 @@ public:
       DynamicJsonBuffer _buffer(json.length());
       JsonObject& _obj = _buffer.parseObject(json.c_str());
       _module = _obj["module"].is<const char*>() ? _obj["module"].as<const char*>() : "";
-      _subModule = _obj["sub"].is<const char*>() ? _obj["sub"].as<const char*>() : "";
+      _subModule = _obj["sub-module"].is<const char*>() ? _obj["sub-module"].as<const char*>() : "";
       _command = _obj["command"].is<const char*>() ? _obj["command"].as<const char*>() : "";
       _data = _obj["data"].is<const char*>() ? _obj["data"].as<const char*>() : "";
     };
@@ -37,4 +37,13 @@ public:
   bool hasData(){ return !this->_data.empty() && this->_data != ""; };
   int getDataLength(){ return this->hasData() ? this->_data.length() : -1; };
   std::string getData(){ return this->_data; };
+  std::string toJSON(){
+    DynamicJsonBuffer _buffer(512);
+    JsonObject& _obj = _buffer.createObject();
+    _obj["module"] = this->_module.c_str();
+    _obj["sub-module"] = this->_subModule.c_str();
+    _obj["command"] = this->_command.c_str();
+    _obj["data"] = this->_data.c_str();
+    return stringify(_obj);
+  };
 };
